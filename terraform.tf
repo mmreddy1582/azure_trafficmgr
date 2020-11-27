3a7b33599ac6f3fb8e76f64c1e6fd3b0ae8e6a37
mmreddy1582 <35883559+mmreddy1582@users.noreply.github.com>
2020-11-27 12:13:03 +0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "TFstate-rg"
-    storage_account_name = "remotestateformstorage"
+    storage_account_name = "remotestateformstorage1"
     container_name       = "mura-tfstate"
     key                  = "web.tfstate"
   }
